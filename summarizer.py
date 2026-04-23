6b3c4213cd812701ddb49c8a304b7c70b168da0e
Mr. Rimanshu patidar <rimanshupatidar046@gmail.com>
2026-04-23 22:03:22 +0530

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,13 +1,18 @@
 from transformers import pipeline
 
+# ✅ MODEL ONLY LOAD ONCE
+summarizer = pipeline(
+    "summarization",
+    model="sshleifer/distilbart-cnn-12-6"
+)
+
 def summarize_text(text):
-    summarizer = pipeline(
-        "summarization",
-        model="sshleifer/distilbart-cnn-12-6"
+    summary = summarizer(
+        text,
+        max_length=100,
+        min_length=30,
+        do_sample=False
     )
-
-    summary = summarizer(text, max_length=100, min_length=30, do_sample=False)
-
     return summary[0]['summary_text']
 
 def summarize_text(text):
